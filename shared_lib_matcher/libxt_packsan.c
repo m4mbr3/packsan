4dcfa2861a0208501fbbbd698003db811f4f7adf
alby.scola@gmail.com <alby.scola@gmail.com@3b993b5e-523c-29d0-2d20-f071bf92adb0>
2011-09-07 11:20:25 +0000

    aggiunta documentazione

--- shared_lib_matcher/libxt_packsan.c
+++ shared_lib_matcher/libxt_packsan.c
@@ -1,25 +1,45 @@
+/*! \file libxt_packsan.c
+ * \brief interface between user space and matcher module
+ */
+
 #include <xtables.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <getopt.h>
 #include <string.h>
 
-
+/*! \var struct option packsan_mt_opts[]
+ * \brief list of parameters to be inserted from command line
+ */
 static const struct option packsan_mt_opts[] = {
   {NULL},
   };
 
+/*! \fn int packsan_mt4_parse(int c, char **argv, int invert, unsigned int *flags, const void *entry, struct xt_entry_match **match)
+ * \brief checks the parameters passed to the module
+ * \param c character passed to identify the option
+ * \param argv list of arguments
+ * \param flags bitmask to compare with local parameters
+ * \return 0 if some option is not correct, nonzero otherwise
+ */
 static int packsan_mt4_parse(int c, char **argv, int invert, unsigned int *flags, const void *entry, struct xt_entry_match **match){
   /* We have no extra-option then we no parse anything */
   return true;
 }
 
+/*! \fn void packsan_mt_check(unsigned int flags)
+ * \brief checks the passed flags
+ * \param flags the flags passed by users
+ */
 static void packsan_mt_check(unsigned int flags)
 {
   /*We have no extra-option then we no check anything*/
 }
 
 
+/*! \fn void packsan_mt4_print(const void *entry, const struct xt_entry_match *match, int numeric)
+ * \brief prints a message once the rule has been inserted (like options or caveats)
+ */
 static void packsan_mt4_print(const void *entry,
 			     const struct xt_entry_match *match, int numeric)
 {
@@ -27,12 +47,18 @@ static void packsan_mt4_print(const void *entry,
 }
 
 
+/*! \fn void packsan_mt_help(void)
+ * \brief prints a help message
+ */
 static void packsan_mt_help(void){
   printf("\nPacksan match options:\n"
 	 "\n[!] At the moment, No option avaiable  \n"
 	 );
 }
 
+/*! \var struct xtables_match packsan_mt4_reg
+ * \brief struct to register the plugin
+ */
 static struct xtables_match packsan_mt4_reg = {
   .version 		= XTABLES_VERSION,
   .name 		= "packsan",
@@ -47,6 +73,9 @@ static struct xtables_match packsan_mt4_reg = {
   .print 		= packsan_mt4_print,
 };
 
+/*! \fn void _init(void)
+ * \brief function called at the insertion to register the module
+ */
 void _init(void)
 {
   xtables_register_match(&packsan_mt4_reg);
--- shared_lib_target/libxt_PACKSAN.c
+++ shared_lib_target/libxt_PACKSAN.c
@@ -1,33 +1,62 @@
+/*! \file libxt_packsan.c
+ * \brief interface between user space and target module
+ */
+
 #include <xtables.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <getopt.h>
 
-static const struct option_packsan_tg opts[]= {
+
+/*! \var struct option packsan_tg_opts[]
+ * \brief list of parameters to be inserted from command line
+ */
+static const struct option packsan_tg_opts[]= {
   {NULL},
 };
 
+/*! \fn int packsan_tg_parse(int c, char **argv, int invert, unsigned int *flags, const void *entry, struct xt_entry_match **match)
+ * \brief checks the parameters passed to the module
+ * \param c character passed to identify the option
+ * \param argv list of arguments
+ * \param flags bitmask to compare with local parameters
+ * \return 0 if some option is not correct, nonzero otherwise
+ */
 static int packsan_tg4_parse(int c, char **argv, int invert, unsigned int *flags, const void *entry, struct xt_entry_match **match){
   /* We have no extra-option then we no parse anything */
   return true;
 }
 
+/*! \fn void packsan_tg_check(unsigned int flags)
+ * \brief checks the passed flags
+ * \param flags the flags passed by users
+ */
 static void packsan_tg_check(unsigned int flags)
 {
     /* We have no extra-option then we no check anything*/
 }
 
+/*! \fn void packsan_tg_print(const void *entry, const struct xt_entry_match *match, int numeric)
+ * \brief prints a message once the rule has been inserted (like options or caveats)
+ */
 static void packsan_tg4_print(const void *entry,
 					const struct xt_entry_match *match, int numeric)
 {
   printf("\n[!] No option to print...\n");
 }
 
+/*! \fn void packsan_tg_help(void)
+ * \brief prints a help message
+ */
 static void packsan_tg_help(void){
   printf("\nPacksan match options:\n"
     "\n[!] At the momente, No option avaiable \n"
 	    );
 }
 
+/*! \var struct xtables_target packsan_tg_reg
+ * \brief struct to register the plugin
+ */
 static struct xtables_target packsan_tg4_reg = {
   .version 		= XTABLES_VERSION,
   .name		= "PACKSAN",
@@ -42,6 +71,9 @@ static struct xtables_target packsan_tg4_reg = {
   .print		= packsan_tg4_print,
 };
 
+/*! \fn void _init(void)
+ * \brief function called at the insertion to register the module
+ */
 void _init(void)
 {
   xtables_register_target(&packsan_tg4_reg);
