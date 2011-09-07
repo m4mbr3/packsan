d07dd2a32f7734a031401084ba00af1b2c4396b4
mambro007@gmail.com <mambro007@gmail.com@3b993b5e-523c-29d0-2d20-f071bf92adb0>
2011-09-07 12:20:33 +0000

    Pulizia dai file compilati

--- Target/.built-in.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/Andrea/Documents/C/Kernel_module/packsan/packsan/Target/built-in.o :=  rm -f /home/Andrea/Documents/C/Kernel_module/packsan/packsan/Target/built-in.o; ar rcs /home/Andrea/Documents/C/Kernel_module/packsan/packsan/Target/built-in.o
